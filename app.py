f4c706bd574c093b4d0ab4e86615b0b82bd8981b
Furlos <levprimak1007@gmail.com>
2025-04-19 09:51:56 +0300

    front

--- app.py
+++ app.py
@@ -118,8 +118,7 @@ def delete_note(id):
         flash('Note deleted!', 'success')
     return redirect(url_for('index'))
 
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
